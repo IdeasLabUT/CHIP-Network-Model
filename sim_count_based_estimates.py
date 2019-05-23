1430f9efddf4182a0bf8cc8a00c462b5cb924f73
Makan Arastuie <makan.arastuie@gmail.com>
2019-05-23 12:17:35 -0400

    Update sims

--- sim_count_based_estimates.py
+++ sim_count_based_estimates.py
@@ -9,6 +9,7 @@ from parameter_estimation import estimate_hawkes_from_counts
 result_file_path = '/shared/Results/CommunityHawkes/pickles/count_based_estimate'
 
 estimate_alpha_beta = False
+plot_only = True
 
 # sim params
 end_time = 100
@@ -46,6 +47,8 @@ def calc_mean_and_error_of_count_estiamte(n_nodes):
     return bp_mu, bp_alpha_beta_ratio
 
 
+no_alpha_name = "_no_alpha" if not estimate_alpha_beta else ""
+
 true_ratio = alpha/beta
 true_mu = mu_diag
 
@@ -59,48 +62,50 @@ ratio_mse = []
 
 alpha_mse = []
 beta_mse = []
+if not plot_only:
+    for j, n_nodes in enumerate(num_nodes_to_test):
+        results = Parallel(n_jobs=n_cores)(delayed(calc_mean_and_error_of_count_estiamte)
+                                           (n_nodes) for i in range(num_simulations))
 
-for j, n_nodes in enumerate(num_nodes_to_test):
-    results = Parallel(n_jobs=n_cores)(delayed(calc_mean_and_error_of_count_estiamte)
-                                       (n_nodes) for i in range(num_simulations))
+        print(f"Done simulations with {n_nodes} nodes.")
 
-    print(f"Done simulations with {n_nodes} nodes.")
+        results = np.asarray(results, dtype=np.float)
 
-    results = np.asarray(results, dtype=np.float)
+        if estimate_alpha_beta:
+            results = np.reshape(results, (num_simulations, 4))
+        else:
+            results = np.reshape(results, (num_simulations, 2))
 
-    if estimate_alpha_beta:
-        results = np.reshape(results, (num_simulations, 4))
-    else:
-        results = np.reshape(results, (num_simulations, 2))
+        all_estimates[j, :, :] = results
 
-    all_estimates[j, :, :] = results
+        mu_mse.append(np.mean(np.power(results[:, 0] - true_mu, 2)))
+        ratio_mse.append(np.mean(np.power(results[:, 1] - true_ratio, 2)))
 
-    mu_mse.append(np.mean(np.power(results[:, 0] - true_mu, 2)))
-    ratio_mse.append(np.mean(np.power(results[:, 1] - true_ratio, 2)))
-
-    if estimate_alpha_beta:
-        alpha_mse.append(np.mean(np.power(results[:, 2] - alpha, 2)))
-        beta_mse.append(np.mean(np.power(results[:, 3] - beta, 2)))
+        if estimate_alpha_beta:
+            alpha_mse.append(np.mean(np.power(results[:, 2] - alpha, 2)))
+            beta_mse.append(np.mean(np.power(results[:, 3] - beta, 2)))
 
 
-no_alpha_name = "_no_alpha" if not estimate_alpha_beta else ""
-# Save results
-with open(f'{result_file_path}/all_estimates{no_alpha_name}.pckl', 'wb') as handle:
-    pickle.dump(all_estimates, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    no_alpha_name = "_no_alpha" if not estimate_alpha_beta else ""
+    # Save results
+    with open(f'{result_file_path}/all_estimates{no_alpha_name}.pckl', 'wb') as handle:
+        pickle.dump(all_estimates, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 with open(f'{result_file_path}/all_estimates{no_alpha_name}.pckl', 'rb') as handle:
     all_estimates = pickle.load(handle)
 
 
 if estimate_alpha_beta:
-    with open(f'{result_file_path}/mses.pckl', 'wb') as handle:
-        pickle.dump([mu_mse, ratio_mse, alpha_mse, beta_mse], handle, protocol=pickle.HIGHEST_PROTOCOL)
+    if not plot_only:
+        with open(f'{result_file_path}/mses.pckl', 'wb') as handle:
+            pickle.dump([mu_mse, ratio_mse, alpha_mse, beta_mse], handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     with open(f'{result_file_path}/mses.pckl', 'rb') as handle:
         mu_mse, ratio_mse, alpha_mse, beta_mse = pickle.load(handle)
 else:
-    with open(f'{result_file_path}/mses_no_alpha.pckl', 'wb') as handle:
-        pickle.dump([mu_mse, ratio_mse], handle, protocol=pickle.HIGHEST_PROTOCOL)
+    if not plot_only:
+        with open(f'{result_file_path}/mses_no_alpha.pckl', 'wb') as handle:
+            pickle.dump([mu_mse, ratio_mse], handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     with open(f'{result_file_path}/mses_no_alpha.pckl', 'rb') as handle:
         mu_mse, ratio_mse = pickle.load(handle)
@@ -119,20 +124,30 @@ if estimate_alpha_beta:
     print("\nBeta MSE:")
     print(beta_mse)
 
-
 plt.bar(range(len(num_nodes_to_test)), mu_mse)
-plt.title("MSE of Mu estimate using count matrix")
-plt.xlabel("Number of Nodes")
-plt.ylabel("Mean Squared Error")
+
+# plt.title("MSE of Mu estimate using count matrix")
+plt.xlabel("Number of Nodes", fontsize=16)
+plt.ylabel("Mean Squared Error", fontsize=16)
 plt.xticks(range(len(num_nodes_to_test)), num_nodes_to_test)
-plt.show()
+plt.tick_params(labelsize=12)
+plt.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+plt.autoscale()
+plt.savefig(f"{result_file_path}/plots/mu_mse.pdf")
+# plt.show()
+
+plt.clf()
 
 plt.bar(range(len(num_nodes_to_test)), ratio_mse)
-plt.title("MSE of m estimate using count matrix")
-plt.xlabel("Number of Nodes")
-plt.ylabel("Mean Squared Error")
+# plt.title("MSE of m estimate using count matrix")
+plt.xlabel("Number of Nodes", fontsize=16)
+plt.ylabel("Mean Squared Error", fontsize=16)
 plt.xticks(range(len(num_nodes_to_test)), num_nodes_to_test)
-plt.show()
+plt.tick_params(labelsize=12)
+plt.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+plt.autoscale()
+plt.savefig(f"{result_file_path}/plots/m_mse.pdf")
+# plt.show()
 
 if estimate_alpha_beta:
     plt.bar(range(len(num_nodes_to_test)), alpha_mse)
