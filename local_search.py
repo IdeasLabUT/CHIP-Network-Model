6f913aba382fb8fe42dcba64a60186bcf6cec250
Makan Arastuie <makan.arastuie@gmail.com>
2019-06-05 02:07:29 -0400

    Add parallelization for local search

--- local_search.py
+++ local_search.py
@@ -1,13 +1,112 @@
 import time
 import numpy as np
+import multiprocessing
+from joblib import Parallel, delayed
+import generative_model_utils as utils
+import parameter_estimation as estimate_utils
 from sklearn.metrics import adjusted_rand_score
 from spectral_clustering import spectral_cluster
-import parameter_estimation as estimate_utils
-import generative_model_utils as utils
 import community_model_fitting_utils as fit_utils
 
 
-def chp_local_search(event_dict, n_classes, node_membership_init, duration, max_iter=100, verbose=True):
+def calc_node_neigh_solutions(event_dict, n_classes, duration, node_membership, agg_adj, beta, log_lik_init, n_i):
+    """
+    Calculates the log-likelihood of neighboring solutions of a single node by changing its membership. If a higher
+    log-likelihood was achieved the best solution will be returned, else a tuple of three np.nan is returned.
+    :return: (node index, best class index, log_likelihood)
+    """
+
+    n_i_class = node_membership[n_i]
+    best_neigh = (np.nan, np.nan, np.nan)
+    log_lik = log_lik_init
+    node_membership = node_membership.copy()
+
+    for c_i in range(n_classes):
+        if c_i == n_i_class:
+            continue
+
+        # update node_membership temporarily
+        node_membership[n_i] = c_i
+
+        # Eval the aprox log_lik of this neighbor, by est its mu and alpha/beta and using previous beta.
+        neigh_mu, neigh_alpha_beta_ratio = estimate_utils.estimate_hawkes_from_counts(agg_adj, node_membership,
+                                                                                      duration,
+                                                                                      default_mu=1e-10 / duration)
+        neigh_alpha = neigh_alpha_beta_ratio * beta
+
+        block_pair_events = utils.event_dict_to_block_pair_events(event_dict, node_membership, n_classes)
+        neigh_log_lik = fit_utils.calc_full_log_likelihood(block_pair_events, node_membership,
+                                                           neigh_mu, neigh_alpha, beta,
+                                                           duration, n_classes, add_com_assig_log_prob=False)
+
+        # if log_lik if this neighbor is better than the "so far" best neighbor, use this neighbors as the best.
+        if log_lik < neigh_log_lik:
+            log_lik = neigh_log_lik
+            best_neigh = (n_i, c_i, log_lik)
+
+        node_membership[n_i] = n_i_class
+
+    return best_neigh
+
+
+def chp_local_search(event_dict, n_classes, node_membership_init, duration, max_iter=100, n_cores=-1, verbose=True):
+    n_nodes = len(node_membership_init)
+    node_membership = node_membership_init
+    agg_adj = utils.event_dict_to_aggregated_adjacency(n_nodes, event_dict, dtype=np.int)
+
+    # estimate initial params of CHP and its log-likelihood
+    (mu,
+     alpha,
+     beta,
+     alpha_beta_ratio) = fit_utils.estimate_bp_hawkes_params(event_dict, node_membership, duration, n_classes)
+
+    block_pair_events = utils.event_dict_to_block_pair_events(event_dict, node_membership, n_classes)
+    init_log_lik = fit_utils.calc_full_log_likelihood(block_pair_events, node_membership, mu, alpha, beta,
+                                                      duration, n_classes, add_com_assig_log_prob=False)
+
+    log_lik = init_log_lik
+    n_cores = n_cores if n_cores > 0 else multiprocessing.cpu_count()
+    print(n_cores)
+    for iter in range(max_iter):
+        if verbose:
+            print(f"Iteration {iter}...", end='\r')
+
+        # for each of the (k-1)*n neighboring solutions
+        possible_solutions = Parallel(backend="threading", n_jobs=n_cores)(delayed(calc_node_neigh_solutions)
+                                                      (event_dict, n_classes, duration, node_membership, agg_adj,
+                                                       beta, log_lik, n_i)
+                                                      for n_i in range(n_nodes))
+
+        possible_solutions = np.array(possible_solutions)
+
+        # if all returned log-likelihoods are np.nan, break. You're at a local optima.
+        if np.all(np.isnan(possible_solutions[:, 2])):
+            if verbose:
+                print(f"Local solution found with {iter} iterations.")
+            break
+
+        max_ll_neigh_idx = np.nanargmax(possible_solutions[:, 2])
+
+        # if a good neighbor was found, update all CHP params, and go for the next iteration.
+        node_membership[int(possible_solutions[max_ll_neigh_idx, 0])] = int(possible_solutions[max_ll_neigh_idx, 1])
+        (mu,
+         alpha,
+         beta,
+         alpha_beta_ratio) = fit_utils.estimate_bp_hawkes_params(event_dict, node_membership, duration, n_classes)
+
+        block_pair_events = utils.event_dict_to_block_pair_events(event_dict, node_membership, n_classes)
+        log_lik = fit_utils.calc_full_log_likelihood(block_pair_events, node_membership, mu, alpha, beta,
+                                                     duration, n_classes, add_com_assig_log_prob=False)
+
+    if verbose:
+        print(f"likelihood went from {init_log_lik:.4f} to {log_lik:.4f}. "
+              f"{100 * np.abs((log_lik - init_log_lik) / init_log_lik):.2f}% increase.")
+
+    return node_membership
+
+
+# This function is only here for speed comparisons.
+def chp_local_search_single_core(event_dict, n_classes, node_membership_init, duration, max_iter=100, verbose=True):
     n_nodes = len(node_membership_init)
     node_membership = node_membership_init
     agg_adj = utils.event_dict_to_aggregated_adjacency(n_nodes, event_dict, dtype=np.int)
@@ -38,16 +137,13 @@ def chp_local_search(event_dict, n_classes, node_membership_init, duration, max_
             for c_i in range(n_classes):
                 if c_i == n_i_class:
                     continue
-
-                tic = time.time()
-
                 # update node_membership temporarily
                 node_membership[n_i] = c_i
 
                 # Eval the aprox log_lik of this neighbor, by est its mu and alpha/beta and using previous beta.
                 neigh_mu, neigh_alpha_beta_ratio = estimate_utils.estimate_hawkes_from_counts(agg_adj, node_membership,
                                                                                               duration,
-                                                                                              default_mu=1e-10/duration)
+                                                                                              default_mu=1e-10 / duration)
                 neigh_alpha = neigh_alpha_beta_ratio * beta
 
                 block_pair_events = utils.event_dict_to_block_pair_events(event_dict, node_membership, n_classes)
@@ -62,9 +158,6 @@ def chp_local_search(event_dict, n_classes, node_membership_init, duration, max_
 
                 node_membership[n_i] = n_i_class
 
-                toc = time.time()
-                print(toc - tic)
-                
         # if no neighbor seem to increase log_lik, break. You're at a local optima.
         if best_neigh is None:
             if verbose:
@@ -84,7 +177,7 @@ def chp_local_search(event_dict, n_classes, node_membership_init, duration, max_
 
     if verbose:
         print(f"likelihood went from {init_log_lik:.4f} to {log_lik:.4f}. "
-              f"{100 * np.abs(log_lik - init_log_lik) / init_log_lik:.2f}% increase.")
+              f"{100 * np.abs((log_lik - init_log_lik) / init_log_lik):.2f}% increase.")
 
     return node_membership
 
@@ -145,8 +238,8 @@ def calc_bp_size(class_size, neigh_switch=None):
 
 if __name__ == '__main__':
     n_classes = 4
-    n_nodes = 64
-    duration = 200
+    n_nodes = 265
+    duration = 400
 
     params = {'number_of_nodes': n_nodes,
               'alpha': 0.6,
@@ -166,8 +259,22 @@ if __name__ == '__main__':
     sc_rand = adjusted_rand_score(true_class_assignments, spectral_node_membership)
     print(f"SC Rand index: {sc_rand:.3f}")
 
+    print("Parallel")
+    tic = time.time()
     local_search_node_membership = chp_local_search(event_dict, n_classes, spectral_node_membership, duration,
-                                                    max_iter=20, verbose=True)
+                                                    max_iter=1, verbose=True)
+    toc = time.time()
+    print(f"local search took {toc - tic:.2f}s.")
 
+    sc_rand = adjusted_rand_score(true_class_assignments, local_search_node_membership)
+    print(f"Local search Rand index: {sc_rand:.3f}")
+
+
+    print("Single core")
+    tic = time.time()
+    local_search_node_membership = chp_local_search_single_core(event_dict, n_classes, spectral_node_membership,
+                                                                duration, max_iter=1, verbose=True)
+    toc = time.time()
+    print(f"local search took {toc - tic:.2f}s.")
     sc_rand = adjusted_rand_score(true_class_assignments, local_search_node_membership)
     print(f"Local search Rand index: {sc_rand:.3f}")
