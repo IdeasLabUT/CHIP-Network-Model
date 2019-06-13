774b72e30dacd07cbd6163140acc7dbd8be0e1f7
Makan Arastuie <makan.arastuie@gmail.com>
2019-06-13 16:20:06 -0400

    Update AS3 with plotting and testing values

--- sim_AS3.py
+++ sim_AS3.py
@@ -6,6 +6,16 @@ import generative_model_utils as utils
 from sklearn.metrics import adjusted_rand_score
 from spectral_clustering import spectral_cluster
 
+'''
+** simulation AS3: fix parameter values, draw a heatmap in each case. The following setup is such that the lower left 
+corner will be the hard regime, so highest mis-clustering rate and and right top corner will be the easy regime with 
+lower mis-clustering rate. 
+
+(a) fix $n$ increasing $T$ and decreasing $k$.
+(b) fix $T$, increasing $n$ and decreasing $k$. 
+(c) fix $k$, increasing $n$ and increasing $T$.
+'''
+
 
 def heatmap(data, row_labels, col_labels, ax=None,
             cbar_kw={}, cbarlabel="", **kwargs):
@@ -39,14 +49,14 @@ def heatmap(data, row_labels, col_labels, ax=None,
 
     # Create colorbar
     cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
-    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom", fontsize=16)
 
     # We want to show all ticks...
     ax.set_xticks(np.arange(data.shape[1]))
     ax.set_yticks(np.arange(data.shape[0]))
     # ... and label them with the respective list entries.
-    ax.set_xticklabels(col_labels)
-    ax.set_yticklabels(row_labels)
+    ax.set_xticklabels(col_labels, fontsize=12)
+    ax.set_yticklabels(row_labels, fontsize=12)
 
     # Turn spines off and create white grid.
     for edge, spine in ax.spines.items():
@@ -60,43 +70,46 @@ def heatmap(data, row_labels, col_labels, ax=None,
     return im, cbar
 
 
+def test_spectral_clustering_on_generative_model(n, t, k):
+    params = {'number_of_nodes': n,
+              'end_time': t,
+              'class_probabilities': np.ones(k) / k,
+              'alpha': 0.05,
+              'beta': 0.08,
+              'mu_diag': 0.075,
+              'mu_off_diag': 0.055,
+              'scale': False}
+
+    event_dict, true_class_assignments = utils.simulate_community_hawkes(params)
+
+    # Spectral clustering on aggregated adjacency matrix
+    agg_adj = utils.event_dict_to_aggregated_adjacency(len(true_class_assignments), event_dict)
+    agg_adj_pred = spectral_cluster(agg_adj, num_classes=k)
+    agg_adj_sc_rand = adjusted_rand_score(true_class_assignments, agg_adj_pred)
+
+    return agg_adj_sc_rand
+
+
 result_file_path = '/shared/Results/CommunityHawkes/pickles/AS3'
 
 plot_only = False
 
-fixed_var = 'n'
-# fixed_var = 't'
-# fixed_var = 'k'
+# Number of test values for all variable must be the same
+n_range = [2048, 1024, 512, 256, 128, 64]
+t_range = [1024, 512, 256, 128, 64, 32]
+k_range = [12, 10, 8, 6, 4, 2]
+
+num_test_values = len(n_range)
+
+fixed_n = 64
+fixed_t = 64
+fixed_k = 4
+
+num_simulation_per_duration = 34
+n_cores = 34
 
 for fixed_var in ['n', 't', 'k']:
-    # Number of test values for all variable must be the same
-    n_range = [16, 32, 64, 128, 256, 512]
-    t_range = [10, 20, 50, 100, 250, 500]
-    k_range = [15, 10, 5, 3, 2, 1]
-    num_test_values = len(n_range)
-
-    fixed_n = 32
-    fixed_t = 50
-    fixed_k = 4
-
-    num_simulation_per_duration = 34
-    n_cores = 34
-
-
-    def test_spectral_clustering_on_generative_model(n, t, k):
-        params = {'number_of_nodes': n,
-                  'end_time': t,
-                  'class_probabilities': np.ones(k) / k}
-
-        event_dict, true_class_assignments = utils.simulate_community_hawkes(params)
-
-        # Spectral clustering on aggregated adjacency matrix
-        agg_adj = utils.event_dict_to_aggregated_adjacency(len(true_class_assignments), event_dict)
-        agg_adj_pred = spectral_cluster(agg_adj, num_classes=k)
-        agg_adj_sc_rand = adjusted_rand_score(true_class_assignments, agg_adj_pred)
-
-        return agg_adj_sc_rand
-
+    print("Fixing:", fixed_var)
 
     mean_sc_rand_scores = []
     mean_sc_rand_scores_err = []
@@ -110,7 +123,8 @@ for fixed_var in ['n', 't', 'k']:
     elif fixed_var == 't':
         t_range_to_test = [fixed_t]
     else:
-        k_range_to_test = [k_range]
+        k_range_to_test = [fixed_k]
+        t_range_to_test = t_range_to_test[::-1]
 
     if not plot_only:
         mean_sc_rand_scores = []
@@ -141,11 +155,9 @@ for fixed_var in ['n', 't', 'k']:
     with open(f'{result_file_path}/all_sims-fixed-{fixed_var}.pckl', 'rb') as handle:
         [mean_sc_rand_scores, mean_sc_rand_scores_err] = pickle.load(handle)
 
-
     print(f"community model fixed {fixed_var}:")
     print(f"rand:", mean_sc_rand_scores)
     print(f"rand error:", mean_sc_rand_scores_err)
-
 
     # Plot Results
     fig, ax = plt.subplots()
@@ -153,23 +165,28 @@ for fixed_var in ['n', 't', 'k']:
     if fixed_var == 'n':
         ylables = t_range
         xlables = k_range
-        plt.ylabel("T")
-        plt.xlabel("K")
+        xlab = "K"
+        ylab = "T"
 
     elif fixed_var == 't':
         ylables = n_range
         xlables = k_range
-        plt.ylabel("N")
-        plt.xlabel("K")
+        xlab = "K"
+        ylab = "N"
 
     else:
         ylables = n_range
         xlables = t_range
-        plt.ylabel("N")
-        plt.xlabel("T")
+        xlab = "T"
+        ylab = "N"
 
-    im, _ = heatmap(mean_sc_rand_scores, ylables, xlables, ax=ax, cmap="Greys", cbarlabel=f"Fixed {fixed_var}")
+    im, _ = heatmap(mean_sc_rand_scores, ylables, xlables, ax=ax, cmap="coolwarm",
+                    cbarlabel=f"Spectral Clustering Adjusted Rand Index")
 
-    ax.set_title(f"AS3 Fixed {fixed_var} Community Model SC Rand")
+    plt.ylabel(ylab, fontsize=16)
+    plt.xlabel(xlab, fontsize=16)
+    ax.set_title(f"CHP AS3 Fixed {fixed_var}")
     fig.tight_layout()
-    plt.show()
+    plt.savefig(f"{result_file_path}/plots/as3-fixed-{fixed_var}.pdf")
+    # plt.show()
+
